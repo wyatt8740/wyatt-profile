cf589ace69071d87598b592faef06b95ad1a1423
wyatt8740 <wyattfward@gmail.com>
2018-10-17 12:43:16 -0400

    Added files for udev and policykit rules/authority.

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo "After that, we will be using sudo and therefore you be able to use your us
 echo "password instead."
 echo "Your user's name: ""$USER"
 echo "Running su to install sudo and add ""$USER""to the 'sudo' group on the machine."
-echo "Please enter the root password when prompted."
+echo "Please enter the _root_ password when prompted."
 set -x
 su -c 'apt-get -y install sudo; adduser '"$USER"' sudo'
 set +x
@@ -31,7 +31,7 @@ continueok
 echo "Installing ksh, FVWM, stalonetray, hsetroot, xterm, rxvt, and compton (the bare"
 echo "essentials)."
 set -x
-sudo apt-get -y install ksh fvwm stalonetray hsetroot compton xterm rxvt
+sudo apt-get install ksh fvwm stalonetray hsetroot compton xterm rxvt
 set +x
 continueok
 echo "Installing x11-apps (for xwd/xclock), x11-xserver-utils (xsetroot),  and"
@@ -39,7 +39,7 @@ echo "imagemagick for use with my screenshot-taking GUI program (xwdui). Also"
 echo "installing both versions of Python, of which only one version (2) is strictly"
 echo "necessary, but it's good to have both."
 set -x
-sudo apt-get -y install x11-apps imagemagick x11-xserver-utils python python3 python-tk python3-tk
+sudo apt-get install x11-apps imagemagick x11-xserver-utils python python3 python-tk python3-tk
 set +x
 continueok
 echo "Installing further dependencies of my init scripts. These include xindkeys,"
@@ -47,33 +47,33 @@ echo "pnmixer, nm-tray (network-manager tray applet), mate-power-manager/mate-te
 echo "(and the rest of MATE unfortunately... My environment could use some"
 echo "trimming here)."
 set -x
-sudo apt-get -y install xbindkeys pnmixer nm-tray mate-power-manager mate-terminal
+sudo apt-get install xbindkeys pnmixer nm-tray mate-power-manager mate-terminal
 set +x
 continueok
 echo "Installing SCIM (input method software) and scim-anthy (for CJK input)."
 set -x
-sudo apt-get -y install scim scim-gtk-immodule scim scim-im-agent scim-anthy scim-modules-table scim-modules-socket scim-tables-ja libscim8v5 libanthy1 libanthyinput0 anthy anthy-common kasumi
+sudo apt-get install scim scim-gtk-immodule scim scim-im-agent scim-anthy scim-modules-table scim-modules-socket scim-tables-ja libscim8v5 libanthy1 libanthyinput0 anthy anthy-common kasumi
 set +x
 continueok
 echo "Installing GNU Unifont (a font I like)."
 set -x
-sudo apt-get -y install ttf-unifont unifont psf-unifont xfonts-unifont
+sudo apt-get install ttf-unifont unifont psf-unifont xfonts-unifont
 set +x
 continueok
 echo "Installing xvkbd (on-screen keyboard I mostly like) and xdotool (for"
 echo "automating tasks)."
 set -x
-sudo apt-get -y install xvkbd xdotool
+sudo apt-get install xvkbd xdotool
 set +x
 continueok
 echo "Installing mcomix, my favorite image viewer."
 set -x
-sudo apt-get -y install mcomix
+sudo apt-get install mcomix
 set +x
 continueok
 echo "Installing Mozilla Firefox, a decent browser until I can get Seamonkey built."
 set -x
-sudo apt-get -y install firefox
+sudo apt-get install firefox
 set +x
 continueok
 echo "Installing some more development-oriented stuff (emacs, binutils, gcc, diff, git,"
@@ -83,7 +83,7 @@ echo "OpenGL (MESA) stuff, X11 stuff, libxml, curses, some compression software,
 echo "development headers for common libraries, etc)."
 echo "This will likely take a while. It's a LOT of stuff."
 set -x
-sudo apt-get -y install git subversion binutils gcc g++ libsdl-image1.2-dev \
+sudo apt-get install git subversion binutils gcc g++ libsdl-image1.2-dev \
      libsdl-mixer1.2-dev libsdl-sound1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev \
      libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
      libgtk2.0-dev libgtk-3-dev libgtk2-perl libgtk3-perl libgtk2.0-bin \
@@ -226,8 +226,47 @@ fi
 continueok
 echo "Downloading Seamonkey EarlyBlue theme to ~/.mozilla so I have it when I"
 echo "need it (this theme matches well with the CDE motif look I'm shooting for)."
+set -x
 wget 'https://addons.thunderbird.net/seamonkey/downloads/latest/earlyblue/addon-3050-latest.xpi' \
      -O ~/.mozilla/
+set +x
 continueok
+echo "Installing Deluge daemon, Deluge GTK client, Deluge console client, and"
+echo "deluge web client."
+set -x
+sudo apt-get install deluged deluge-gtk deluge-console deluge-web
+set +x
+continueok
+# to do: copy policykit rules and udev rules
+# (when not using systemd as an init system, policykit rules are more
+# important. I don't remove systemd-as-init with this script because doing so
+# is a highly involved, volatile, and irritating process in Debian and I don't
+# want to trash my fresh installation by trying to plow ahead with it.)
+# (also, systemd-shim appears to be out of date.)
 
+# udev
+echo "Copying udev rules and hwdb entries to /etc/udev."
+set -x
+cd "$STARTDIR""/etc/udev"
+sudo sh -c 'tar -c -f - hwdb.d/ rules.d/ | tar -C /etc/udev -x -v -f - '
+sudo chown -R root:root /etc/udev
+sudo sh -c 'find /etc/udev/ -type f -print0 | xargs -0 chmod 644'
+sudo sh -c 'find /etc/udev/ -type d -print0 | xargs -0 chmod 755'
+set +x
+cd "$STARTDIR/etc"
+continueok
+echo "Copying policykit rules to /etc/polkit-1 and setting correct permissions"
+# /etc/polkit-1/localauthority dir is only readable/writable by owner (root) on
+# my debian system. Files inside it however are globally readable and owner-
+# writable.
+set -x
+cd "$STARTDIR""/etc/polkit-1"
+mkdir -p /etc/polkit-1
+sudo sh -c 'tar -c -f - rules.d/ localauthority.conf.d/ localauthority/ | tar -C /etc/polkit-1 -x -v -f - '
+sudo chown -R root:root /etc/polkit-1
+sudo sh -c 'find /etc/polkit-1/ -type f -print0 | xargs -0 chmod 644'
+sudo sh -c 'find /etc/polkit-1/ -type d -print0 | xargs -0 chmod 755'
+sudo chmod 700 /etc/polkit-1/localauthority
+set +x
+continueok
 echo "Reached end of setup script! Exiting."
